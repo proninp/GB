eed6557eaca8ca674b61e10f4b1a23f4d5029122
proninp <velninpapro@gmail.com>
2024-10-11 09:25:27 +0300

    GeekBrains - ASP .NET Core Web App - HomeWork01 - 2024.10.11

---
+++ C Sharp Faculty/ASP .NET Core Web App/HomeWork01/HomeWork01/src/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWork01.src;
+public class Post
+{
+    public int UserId { get; set; }
+
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Body { get; set; } = string.Empty;
+
+    public override string ToString() =>
+        $"{UserId}\n{Id}\n{Title}\n{Body}";
+}
---
+++ C Sharp Faculty/ASP .NET Core Web App/HomeWork01/HomeWork01/src/Program.cs
@@ -0,0 +1,30 @@
+﻿using HomeWork01.src;
+using System.Net.Http.Json;
+using System.Text;
+
+var client = new HttpClient();
+
+var startIndex = 4;
+var endIndex = 13;
+var tasks = Enumerable.Range(startIndex, endIndex - startIndex + 1)
+    .Select(i => GetSinglePost(i))
+    .Where(postTask => postTask is not null)
+    .ToList();
+var posts = await Task.WhenAll(tasks);
+
+var fileName = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "posts.txt"));
+await WritePostsToFile(posts, fileName);
+
+async Task<Post?> GetSinglePost(int postId)
+{
+    return await client.GetFromJsonAsync<Post>($"https://jsonplaceholder.typicode.com/posts/{postId}");
+}
+
+async Task WritePostsToFile(Post?[] posts, string fileName)
+{
+    if (posts is null || posts.Length == 0 || string.IsNullOrEmpty(fileName))
+        return;
+
+    var lines = posts.Select(post => post.ToString());
+    await File.WriteAllLinesAsync(path: fileName, contents: lines, encoding: Encoding.UTF8);
+}
